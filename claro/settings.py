1d0fce66251486c4178eb933aaeb4fc7d0741835
Alessandra Oliveira <106682325+itsale-o@users.noreply.github.com>
2026-04-02 08:56:15 -0300

    .

--- claro/settings.py
+++ claro/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 PROD = config('PROD', default=False, cast=bool)
 
